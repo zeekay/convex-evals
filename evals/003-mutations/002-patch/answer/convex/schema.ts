b6e3415b88deb429ab2b277c344e15587f4d484b
Jordan Hunt <65152573+jordanhunt22@users.noreply.github.com>
2025-01-22 18:40:22 -0800

    Patch eval (#11)

---
+++ evals/003-mutations/002-patch/answer/convex/schema.ts
@@ -0,0 +1,10 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  users: defineTable({
+    email: v.string(),
+    name: v.string(),
+    age: v.number(),
+  }).index("by_email", ["email"]),
+});

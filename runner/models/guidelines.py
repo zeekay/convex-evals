a90dd30dca8b3285635f4ffd6bd3beb0903a761c
Jordan Hunt <jordan@convex.dev>
2025-02-10 17:41:19 -0800

    add

--- runner/models/guidelines.py
+++ runner/models/guidelines.py
@@ -280,7 +280,28 @@ CONVEX_GUIDELINES = GuidelineSection(
                     "You can use the helper typescript type `Id` imported from './_generated/dataModel' to get the type of the id for a given table. For example if there is a table called 'users' you can use `Id<'users'>` to get the type of the id for that table."
                 ),
                 Guideline(
-                    "If you need to define a `Record` make sure that you correctly provide the type of the key and value in the type. For example a validator `v.record(v.id('users'), v.string())` would have the type `Record<Id<'users'>, string>`."
+                    """If you need to define a `Record` make sure that you correctly provide the type of the key and value in the type. For example a validator `v.record(v.id('users'), v.string())` would have the type `Record<Id<'users'>, string>`. Below is an example of using `Record` with an `Id` type in a query:
+                    ```ts
+                    import { query } from "./_generated/server";
+                    import { Doc, Id } from "./_generated/dataModel";
+
+                    export const exampleQuery = query({
+                        args: { userIds: v.array(v.id("users")) },
+                        returns: v.record(v.id("users"), v.string()),
+                        handler: async (ctx, args) => {
+                            const idToUsername: Record<Id<"users">, string> = {};
+                            for (const userId of args.userIds) {
+                                const user = await ctx.db.get(userId);
+                                if (user) {
+                                    users[user._id] = user.username;
+                                }
+                            }
+
+                            return idToUsername;
+                        },
+                    });
+                    ```
+                    """
                 ),
                 Guideline(
                     "Be strict with types, particularly around id's of documents. For example, if a function takes in an id for a document in the 'users' table, take in `Id<'users'>` rather than `string`."
